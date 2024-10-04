a03ea3702d521ba5aa2d2741f53b6c1363c1bc09
bohous04 <misalenert@gmail.com>
2024-10-04 13:26:43 +0200

    Added all vanilla images

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+name 'bhd_vehicleimages'
+version '1.0.0'
+author 'BHD SCRIPTS'
+discord 'https://discord.gg/bhdscripts'
+description 'Vehicle images for bhd_garage'
+
+files {
+    'img/*.webp',
+}
